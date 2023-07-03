d6bb9aa6c186ba6691bf90e20b8928e4cf744a10
nyetii <netero725@gmail.com>
2023-07-03 18:40:37 -0300

    Logger service added and existing static services turned into singleton

---
+++ src/OpenGate.Logging/ILog.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenGate.Logging
+{
+    public interface ILog
+    {
+        public void Information(string message);
+        void Warning(string message);
+        void Error(string message);
+        void Fatal(string message);
+        void Debug(string message);
+    }
+}
