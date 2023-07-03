d6a1e42f710bb0514fac915316365f56d8e719d8
nyetii <netero725@gmail.com>
2023-07-02 22:04:35 -0300

    LDAP validation and Test endpoint

---
+++ src/OpenGate/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenGate.Models;
+
+namespace OpenGate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "GetTest")]
+        public IActionResult Get()
+        {
+            var user = new User("opengate.io")
+            {
+                Name = "netty"
+            };
+
+            var isValid = user.Validate("nettypassword");
+
+            if(isValid)
+                return Ok(user);
+
+            return NotFound(user);
+            
+        }
+    }
+}
---
+++ src/OpenGate/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace OpenGate.Models
+{
+    public class User
+    {
+        public readonly string Domain;
+        public readonly int Port;
+
+        public readonly string[] SubDomains;
+
+        public string Name { get; set; } = string.Empty;
+        
+        public string DomainName => this.ToDomainName();
+
+        public string FullId => $"{Name}@{Domain}";
+
+        public User(string domain, int port = 389)
+        {
+            SubDomains = domain.Split('.');
+            Domain = SubDomains[^2];
+            Port = port;
+        }
+
+        public override string ToString() => $"{Name}@{Domain}";
+    }
+}
